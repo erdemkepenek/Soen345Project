fae2d491a7c9de84e72ab977e976c25d4a7b53c7
Anthony Iatropoulos <anthonyiatropoulos@hotmail.com>
2019-03-21 10:28:46 -0400

    [#23] made the forklift class

--- src/main/java/org/springframework/samples/petclinic/Forklifter.java
+++ src/main/java/org/springframework/samples/petclinic/Forklifter.java
@@ -1,8 +1,24 @@
 package org.springframework.samples.petclinic;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Forklifter {
 
-    public void doYouEvenForkLift(){
+    public static void doYouEvenForkLift(){
+        Connection conn = null;
 
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:src/main/resources/db/sqlite/sqlite.db");
+        }catch (SQLException e){
+            System.out.println("Connection failed");
+        }
+
+    }
+
+    public static void main(String[] args){
+        doYouEvenForkLift();
     }
 }

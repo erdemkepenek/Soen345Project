ff10b5b70fc43adc299e2287784113a6477a9f8e
David Bechara <davips96@gmail.com>
2019-03-24 16:03:48 -0400

    #34 Improved ForklifterTests coverage to 94% instructions (100% branches)

--- src/test/java/forklift/ForklifterTests.java
+++ src/test/java/forklift/ForklifterTests.java
@@ -5,23 +5,24 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.ArrayList;
 import org.junit.runner.RunWith;
+
+import java.io.FileNotFoundException;
 import java.lang.reflect.Method;
 import java.sql.*;
 
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 public class ForklifterTests {
 
-    private Connection testNewDb;
-    private Connection testOldDb;
+    private Connection testConn;
+    private Connection testOldConn;
 
     @Before
     public void setup(){
         try {
-            testNewDb = DriverManager.getConnection("jdbc:sqlite:src/main/resources/db/sqlite/sqlite.db");
-            testOldDb = DriverManager.getConnection("jdbc:mysql://localhost:3306/petclinic", "root", "petclinic");
+            testConn = DriverManager.getConnection("jdbc:sqlite:src/main/resources/db/sqlite/sqlite.db");
+            testOldConn = DriverManager.getConnection("jdbc:mysql://eglencecaj.mysql.database.azure.com/petclinic?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "eglen@eglencecaj", "Soen344room3");
         }catch (SQLException e){
             System.out.println("Connection failed");
         }
@@ -30,23 +31,32 @@ public class ForklifterTests {
     @After
     public void teardown(){
         try {
-            testNewDb.close();
-            testOldDb.close();
+            testConn.close();
+            testOldConn.close();
         }catch (SQLException e){
             System.out.println("Connection failed");
         }
     }
 
     @Test
-    public void dropAllTablesTestSuccess() throws Exception {
-        try (PreparedStatement pstmt = testNewDb.prepareStatement("SELECT name FROM sqlite_master WHERE type = 'table'")) {
-            Method method = Forklifter.class.getDeclaredMethod("dropAllTables", Connection.class);
+    public void doYouEvenForkLiftTest() throws Exception {
+        Forklifter.doYouEvenForkLift();
+        ResultSet ownersRs = testConn.createStatement().executeQuery("SELECT * FROM owners");
+        ResultSet petsRs = testConn.createStatement().executeQuery("SELECT * FROM pets");
+        ResultSet visitsRs = testConn.createStatement().executeQuery("SELECT * FROM visits");
+        assertTrue(ownersRs.next() && petsRs.next() && visitsRs.next());
+    }
+
+    @Test
+    public void buildDatabaseTestFalseReset() throws Exception {
+        try {
+            Method method = Forklifter.class.getDeclaredMethod("buildDatabase", Connection.class, boolean.class);
             method.setAccessible(true);
-            method.invoke(Forklifter.class, testNewDb);
-            ResultSet rs = pstmt.executeQuery();
-            assertFalse(rs.next());
+            method.invoke(Forklifter.class, testConn, false);
+            ResultSet rs = testOldConn.createStatement().executeQuery("SELECT table_name FROM information_schema.tables WHERE table_schema = 'petclinic'");
+            assertTrue(rs.next());
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            assertNotNull(e.getMessage());
         }
     }
 
@@ -55,35 +65,35 @@ public class ForklifterTests {
         try {
             Method method = Forklifter.class.getDeclaredMethod("dropAllTables", Connection.class);
             method.setAccessible(true);
-            method.invoke(Forklifter.class, testOldDb);
-            ResultSet rs = testOldDb.createStatement().executeQuery("SELECT table_name FROM information_schema.tables WHERE table_schema = 'petclinic'");
+            method.invoke(Forklifter.class, testOldConn);
+            ResultSet rs = testOldConn.createStatement().executeQuery("SELECT table_name FROM information_schema.tables WHERE table_schema = 'petclinic'");
             assertTrue(rs.next());
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            assertNotNull(e.getMessage());
         }
     }
 
     @Test
-    public void addDataTestSuccess() throws Exception {
+    public void addDataTestWrongDbs() throws Exception {
         try {
             Method method = Forklifter.class.getDeclaredMethod("addData", Connection.class, Connection.class);
             method.setAccessible(true);
-            method.invoke(Forklifter.class, testOldDb, testNewDb);
-            ResultSet ownersRs = testNewDb.createStatement().executeQuery("SELECT * FROM owners");
-            ResultSet petsRs = testNewDb.createStatement().executeQuery("SELECT * FROM pets");
-            ResultSet visitsRs = testNewDb.createStatement().executeQuery("SELECT * FROM visits");
+            method.invoke(Forklifter.class, testConn, testOldConn);
+            ResultSet ownersRs = testOldConn.createStatement().executeQuery("SELECT * FROM owners");
+            ResultSet petsRs = testOldConn.createStatement().executeQuery("SELECT * FROM pets");
+            ResultSet visitsRs = testOldConn.createStatement().executeQuery("SELECT * FROM visits");
             assertTrue(ownersRs.next() && petsRs.next() && visitsRs.next());
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            assertNotNull(e.getMessage());
         }
     }
 
     @Test
-    public void doYouEvenForkLiftTestSuccess() throws Exception {
-        Forklifter.doYouEvenForkLift();
-        ResultSet ownersRs = testNewDb.createStatement().executeQuery("SELECT * FROM owners");
-        ResultSet petsRs = testNewDb.createStatement().executeQuery("SELECT * FROM pets");
-        ResultSet visitsRs = testNewDb.createStatement().executeQuery("SELECT * FROM visits");
-        assertTrue(ownersRs.next() && petsRs.next() && visitsRs.next());
+    public void parseSQLTestNoFilePath() throws Exception {
+        String notAFilePath = "I am not a filepath.";
+        Method method = Forklifter.class.getDeclaredMethod("parseSQL", String.class);
+        method.setAccessible(true);
+        ArrayList<String> testCommand = (ArrayList<String>) method.invoke(Forklifter.class, notAFilePath);
+        assertTrue(testCommand.isEmpty());
     }
 }
